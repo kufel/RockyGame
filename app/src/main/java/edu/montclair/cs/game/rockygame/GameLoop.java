2920fca8260dbf1b068d43d1635e0f942c19d6b8
kufel <sebastian@kufel.net>
2014-10-02 23:47:45 -0400

    clean up

--- app/src/main/java/edu/montclair/cs/game/rockygame/GameLoop.java
+++
@@ -1,7 +0,0 @@
-package edu.montclair.cs.game.rockygame;
-
-/**
- * Created by Sebastian on 9/26/2014.
- */
-public class GameLoop extends Thread {
-}
--- app/src/main/java/edu/montclair/cs/game/rockygame/Settings.java
+++
@@ -1,9 +0,0 @@
-package edu.montclair.cs.game.rockygame;
-
-import android.app.Activity;
-
-/**
- * Created by Sebastian on 9/26/2014.
- */
-public class Settings extends Activity {
-}
